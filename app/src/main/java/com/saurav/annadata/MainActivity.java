cd9da06b08727a48bd90208a4a18fad342358f74
Saurav Patil <saurav250998@gmail.com>
2021-11-19 12:41:45 +0530

    first commit

---
+++ app/src/main/java/com/saurav/annadata/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saurav.annadata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
